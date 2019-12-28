bfc556f5d675db309eea2407d83293d1602932da
王權 <timowang@wangquande-MacBook-Pro.local>
2019-12-28 19:58:36 +0800

    add spread operator

--- src/index.js
+++ src/index.js
@@ -164,9 +164,7 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             };
             users.push(user);
 
@@ -180,10 +178,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             };
 
             posts.push(post);
@@ -203,9 +198,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             };
 
             comments.push(comment);
