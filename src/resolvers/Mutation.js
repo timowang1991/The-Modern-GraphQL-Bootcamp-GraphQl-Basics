403cb26af83e8bf770d65576789ea51d23f6f708
王權 <timowang@wangquande-MacBook-Pro.local>
2019-12-28 21:21:54 +0800

    mutation for update comment and post

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -91,6 +91,26 @@ const Mutation = {
 
         return deletePosts[0];
     },
+    updatePost(parent, { id, data }, { db }, info) {
+        const post = db.posts.find(post => post.id === id);
+        if (!post) {
+            throw new Error('Post not found');
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title;
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body;
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+        }
+
+        return post;
+    },
     createComment(parent, args, { db }, info) {
         const userExists = db.users.some(user => user.id === args.data.author);
         if (!userExists) {
@@ -119,6 +139,18 @@ const Mutation = {
 
         const deleteComments = db.comments.splice(commentIndex, 1);
         return deleteComments[0];
+    },
+    updateComment(parent, { id, data }, { db }, info) {
+        const comment = db.comments.find(comment => comment.id === id);
+        if (!comment) {
+            throw new Error('Comment not found');
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text;
+        }
+
+        return comment;
     }
 };
 
