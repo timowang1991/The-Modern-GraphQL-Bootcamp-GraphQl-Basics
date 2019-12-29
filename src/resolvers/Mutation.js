6f551f1084fab72c5781117c681a9b025dbc63f5
王權 <timowang@wangquande-MacBook-Pro.local>
2019-12-29 16:28:06 +0800

    comment subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -172,20 +172,31 @@ const Mutation = {
         };
 
         db.comments.push(comment);
-        pubsub.publish(`comment ${args.data.post}`, { comment });
+        pubsub.publish(`comment ${args.data.post}`, {
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
+        });
 
         return comment;
     },
-    deleteComment(parent, args, { db }, info) {
+    deleteComment(parent, args, { db, pubsub }, info) {
         const commentIndex = db.comments.findIndex(comment => comment.id === args.id);
         if (commentIndex === -1) {
             throw new Error('Comment not found');
         }
 
-        const deleteComments = db.comments.splice(commentIndex, 1);
-        return deleteComments[0];
+        const [deleteComment] = db.comments.splice(commentIndex, 1);
+        pubsub.publish(`comment ${deleteComment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: deleteComment
+            }
+        });
+        return deleteComment;
     },
-    updateComment(parent, { id, data }, { db }, info) {
+    updateComment(parent, { id, data }, { db, pubsub }, info) {
         const comment = db.comments.find(comment => comment.id === id);
         if (!comment) {
             throw new Error('Comment not found');
@@ -194,6 +205,13 @@ const Mutation = {
         if (typeof data.text === 'string') {
             comment.text = data.text;
         }
+
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        });
 
         return comment;
     }
