feedd4d1946630b2dd0b061425615795e8c7e712
王權 <timowang@wangquande-MacBook-Pro.local>
2019-12-29 14:04:46 +0800

    post subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -64,7 +64,7 @@ const Mutation = {
 
         return user;
     },
-    createPost(parent, args, { db }, info) {
+    createPost(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some(user => user.id === args.data.author);
         if (!userExists) {
             throw new Error('User not found');
@@ -76,6 +76,10 @@ const Mutation = {
         };
 
         db.posts.push(post);
+
+        if (args.data.published) {
+            pubsub.publish('post', { post });
+        }
 
         return post;
     },
